7f4c0c697ab628837e17f7e7bd58768884c8e920
Roopa <roopab77@yahoo.com>
2018-11-08 23:54:01 -0500

    save

--- public/app.js
+++ public/app.js
@@ -1,6 +1,58 @@
 // Grab the articles as a json
 
 $("#getcategories").on("click", function () {
+  $.get("/scrape/category/1", function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      $("#categories").append("<p data-id='" + data[i]._id + "'>" + data[i].category + " - " + data[i]._id + "</p>");
+      
+      var url = "/scrape/book/" +  data[i]._id + ":" + data[i].category;
+
+      $.get(url, function (data) {
+        console.log(data);
+      });
+
+    }
+  });
+  $.get("/scrape/category/2", function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      $("#categories").append("<p data-id='" + data[i]._id + "'>" + data[i].category + " - " + data[i]._id + "</p>");
+      
+      var url = "/scrape/book/" +  data[i]._id + ":" + data[i].category;
+
+      $.get(url, function (data) {
+        console.log(data);
+      });
+
+    }
+  });
+  $.get("/scrape/category/3", function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      $("#categories").append("<p data-id='" + data[i]._id + "'>" + data[i].category + " - " + data[i]._id + "</p>");
+      
+      var url = "/scrape/book/" +  data[i]._id + ":" + data[i].category;
+
+      $.get(url, function (data) {
+        console.log(data);
+      });
+
+    }
+  });
+  $.get("/scrape/category/4", function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      $("#categories").append("<p data-id='" + data[i]._id + "'>" + data[i].category + " - " + data[i]._id + "</p>");
+      
+      var url = "/scrape/book/" +  data[i]._id + ":" + data[i].category;
+
+      $.get(url, function (data) {
+        console.log(data);
+      });
+
+    }
+  });
   $.get("/scrape/category/5", function (data) {
     console.log(data);
     for (var i = 0; i < data.length; i++) {
